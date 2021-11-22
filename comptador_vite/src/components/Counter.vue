c59f79e371d919b2ff6632432b3207867180dce1
Tudor <tgoncear@gmail.com>
2021-11-22 17:23:15 +0100

    Versio del comptador amb vite

---
+++ comptador_vite/src/components/Counter.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p>{{counter}}</p>
+    <button @click="incrementar">+</button>
+    <button @click="decrementar">-</button>
+  </div>
+</template>
+<script>
+export default {
+  data: function () {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    incrementar(){
+      this.counter++;
+    },
+    decrementar(){
+      this.counter--;
+    }
+  }
+}
+</script>
---
+++ vue/Counter.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p>{{counter}}</p>
+    <button @click="incrementar">+</button>
+    <button @click="decrementar">-</button>
+  </div>
+</template>
+<script>
+module.exports = {
+  data: function () {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    incrementar(){
+      this.counter++;
+    },
+    decrementar(){
+      this.counter--;
+    }
+  }
+}
+</script>
